401552fa246230331a7c5ae409715ae986544303
Fetico Daniel <danielfetico21@gmail.com>
2025-03-10 11:25:02 +0200

    Added error constants

---
+++ backend/src/constants/error.ts
@@ -0,0 +1,9 @@
+export const ERROR_DIRECTORY_NOT_FOUND = 'Directory not found or access forbidden';
+export const ERROR_FILE_NOT_FOUND = 'File not found or access forbidden';
+export const ERROR_NOT_A_DIRECTORY = 'The provided path is not a directory';
+export const ERROR_READING_DIRECTORY = 'Error reading directory';
+export const ERROR_RETRIEVING_FILE_DETAILS = 'Error retrieving file details';
+export const ERROR_COULD_NOT_RETRIEVE_FILE_DETAILS = 'Could not retrieve file details';
+export const ERROR_PATH_REQUIRED = 'Path is required';
+export const ERROR_UNABLE_TO_LIST_FILES = 'Unable to list files';
+export const ERROR_UNABLE_TO_READ_FILE = 'Unable to read file';
