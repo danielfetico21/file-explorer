0df960958a6bcb59ea5face8cdefc04f5ad88028
Fetico Daniel <danielfetico21@gmail.com>
2025-03-10 10:00:01 +0200

    added the file path endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-import { readdir } from 'fs/promises'
+import { readdir, stat } from 'fs/promises'
 import path from 'path';
+import { Stats } from 'fs';
 
 
 const app: Application = express()
@@ -30,6 +31,34 @@ app.get("/api/files", async (req: Request, res: Response) => {
         console.error('Error reading directory:', error);
         res.status(500).json({ 
             error: 'Unable to list files', 
+            details: error instanceof Error ? error.message : String(error) 
+        });
+    }
+})
+
+app.get("/api/file", async (req: Request, res: Response) => {
+    const filePath:string = req.query.path as string;
+    
+    if (!filePath) {
+        res.status(400).json({ message: "Path is required" })
+        return
+    }
+
+    try {
+        const fullPath: string = path.resolve(filePath);
+        const getFileStats: Stats = await stat(fullPath);
+        const fileStats = {
+            name: path.basename(fullPath),
+            type: getFileStats.isDirectory() ? 'directory' : 'file',
+            size: getFileStats.size,
+            creationDate: getFileStats.birthtime.toISOString(),
+            lastModified: getFileStats.mtime.toISOString()
+        }
+        res.json({ path: filePath, file: fileStats })
+    } catch (error) {
+        console.error('Error reading file:', error);
+        res.status(500).json({ 
+            error: 'Unable to read file', 
             details: error instanceof Error ? error.message : String(error) 
         });
     }
