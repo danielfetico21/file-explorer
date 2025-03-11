82c884f15289a25caa75a01df48135da08be20c7
Fetico Daniel <danielfetico21@gmail.com>
2025-03-11 17:57:11 +0200

    add platform icons + platfrom badge component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import PlatformBadge from "./components/PlatformBadge";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center w-full p-6">
-      <h1 className="font-poppins font-bold !text-2xl">File Explorer</h1>
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-ce<nter w-full p-6">
+      <div className="flex flex-row items-center justify-center gap-2">
+        <h1 className="font-poppins font-bold !text-2xl">File Explorer</h1>
+        <PlatformBadge />
+      </div>
     </div>
   );
 }
--- frontend/src/components/PlatformBadge.tsx
+++ frontend/src/components/PlatformBadge.tsx
@@ -1,5 +1,43 @@
-const PlatformBadge = () => {
-  return <div>PlatformBadge</div>;
+import { useEffect, useState } from "react";
+import { getPlatform } from "../api/api";
+import LinuxIcon from "../assets/icons/linux.png";
+import MacIcon from "../assets/icons/mac.png";
+import WinIcon from "../assets/icons/windows.png";
+
+const PlatformBadge: React.FC = () => {
+  const [platformName, setPlatformName] = useState<string>("");
+
+  useEffect(() => {
+    getPlatform()
+      .then((data) => setPlatformName(data))
+      .catch((error) => console.error("Error fetching platform", error));
+  }, []);
+
+  const getIconType = (platform: string) => {
+    switch (platform) {
+      case "Windows":
+        return WinIcon;
+      case "MacOS":
+        return MacIcon;
+      default:
+        return LinuxIcon;
+    }
+  };
+
+  if (!platformName) return null;
+
+  return (
+    <div className="inline-flex items-center rounded-md bg-purple-200 px-2 py-1 font-medium ring-1 ring-purple-700/10 ring-inset">
+      <img
+        src={getIconType(platformName)}
+        alt="linux-icon"
+        width={16}
+        height={16}
+        className="mr-1"
+      />
+      <span className="text-purple-700 text-xs">{platformName}</span>
+    </div>
+  );
 };
 
 export default PlatformBadge;
