230821ce8dcc37fc5a6a4666431f74064f84d6d2
Fetico Daniel <danielfetico21@gmail.com>
2025-03-11 23:14:05 +0200

    other small changes + animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import "./App.css";
+import FileExplorer from "./components/FileExplorer";
 import PlatformBadge from "./components/PlatformBadge";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-ce<nter w-full p-6">
-      <div className="flex flex-row items-center justify-center gap-2">
-        <h1 className="font-poppins font-bold !text-2xl">File Explorer</h1>
-        <PlatformBadge />
+    <div className="min-h-screen bg-gray-950 flex flex-col items-center justify-center w-full p-6">
+      <div className="max-w-5xl w-full flex flex-col h-[calc(100vh-3rem)]">
+        <div className="flex flex-row items-center justify-between gap-2 w-full py-4">
+          <div className="font-poppins font-bold text-xl text-white">
+            File Explorer
+          </div>
+          <PlatformBadge />
+        </div>
+
+        <div className="flex-grow w-full bg-gray-900/60 rounded-lg p-4 shadow-lg overflow-hidden">
+          <FileExplorer />
+        </div>
       </div>
     </div>
   );
